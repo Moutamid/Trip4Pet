f526d4c0a5552ec97a4f872d8290efbcb664239b
moutamid <lumianokia188@gmail.com>
2024-04-30 17:18:07 +0500

    commit

--- app/src/main/java/com/moutamid/trip4pet/SplashActivity.java
+++ app/src/main/java/com/moutamid/trip4pet/SplashActivity.java
@@ -30,7 +30,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
             } else {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
                 finish();
             }
         }, 2000);
