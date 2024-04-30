2df1b7882a32c3f3f903c0f6298cc99357b430ab
moutamid <lumianokia188@gmail.com>
2024-04-30 13:36:27 +0500

    commit

--- app/src/main/java/com/moutamid/trip4pet/adapters/LocationAdapter.java
+++ app/src/main/java/com/moutamid/trip4pet/adapters/LocationAdapter.java
@@ -1,14 +1,46 @@
 package com.moutamid.trip4pet.adapters;
 
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moutamid.trip4pet.models.LocationsModel;
+
 import java.util.ArrayList;
 
-public class LocationAdapter {
-    public String name, country, city, description, parkingCost, contact, typeOfPlace;
-    public double latitude, longitude;
-    public ArrayList<String> images;
-    public ArrayList<String> activities;
-    public ArrayList<String> services;
-    public long timestamp;
-    public LocationAdapter() {
+public class LocationAdapter extends RecyclerView.Adapter<LocationAdapter.LocationVH> {
+    Context context;
+    ArrayList<LocationsModel> list;
+
+    public LocationAdapter(Context context, ArrayList<LocationsModel> list) {
+        this.context = context;
+        this.list = list;
     }
+
+    @NonNull
+    @Override
+    public LocationVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LocationVH holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class LocationVH extends RecyclerView.ViewHolder{
+
+        public LocationVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
--- app/src/main/java/com/moutamid/trip4pet/models/LocationsModel.java
+++ app/src/main/java/com/moutamid/trip4pet/models/LocationsModel.java
@@ -1,4 +1,30 @@
 package com.moutamid.trip4pet.models;
 
+import java.util.ArrayList;
+
 public class LocationsModel {
+    public String name, country, city, description, parkingCost, contact, typeOfPlace;
+    public double latitude, longitude;
+    public ArrayList<String> images;
+    public ArrayList<Integer> activities;
+    public ArrayList<Integer> services;
+    public long timestamp;
+    public int rating;
+
+    public LocationsModel(String name, String country, String city, String description, String parkingCost, String contact, String typeOfPlace, double latitude, double longitude, ArrayList<String> images, ArrayList<Integer> activities, ArrayList<Integer> services, long timestamp, int rating) {
+        this.name = name;
+        this.country = country;
+        this.city = city;
+        this.description = description;
+        this.parkingCost = parkingCost;
+        this.contact = contact;
+        this.typeOfPlace = typeOfPlace;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.images = images;
+        this.activities = activities;
+        this.services = services;
+        this.timestamp = timestamp;
+        this.rating = rating;
+    }
 }
